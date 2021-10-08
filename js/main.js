9bd36c303fa8c50407375e7fd229be4b60f1f05e
akirishi <90563712+akirishi@users.noreply.github.com>
2021-10-08 18:12:31 -0300

    Aula 5 - final
    Final do projeto

--- js/main.js
+++ js/main.js
@@ -32,18 +32,25 @@ function criarPonto() {
 document.addEventListener('keydown', update);
 
 function update(event) {
-  if(event.keyCode == 37 && direction != 'right') direction = "left";
-  if(event.keyCode == 38 && direction != 'down') direction = "up";
-  if(event.keyCode == 39 && direction != 'left') direction = "right";
-  if(event.keyCode == 40 && direction != 'up') direction = "down";
+  if (event.keyCode == 37 && direction != 'right') direction = "left";
+  if (event.keyCode == 38 && direction != 'down') direction = "up";
+  if (event.keyCode == 39 && direction != 'left') direction = "right";
+  if (event.keyCode == 40 && direction != 'up') direction = "down";
 }
 
 function start() {
 
-  if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
-  if(snake[0].x < 0 && direction == 'left') snake[0].x = 15 * box;
-  if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
-  if(snake[0].y < 0 && direction == 'up') snake[0].y = 15 * box
+  if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+  if (snake[0].x < 0 && direction == 'left') snake[0].x = 15 * box;
+  if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+  if (snake[0].y < 0 && direction == 'up') snake[0].y = 15 * box;
+
+  for (i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearInterval(jogo)
+      alert('You Loose')
+    }
+  }
 
   criarBG();
   criarSnake();
@@ -57,8 +64,13 @@ function start() {
   if (direction == "up") snakeY -= box;
   if (direction == "down") snakeY += box;
 
-  snake.pop();
-
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  }else {
+    food.x = Math.floor (Math.random() * 15 + 1) * box;
+    food.y = Math.floor (Math.random() * 15 + 1) * box;
+  }
+  
   let newHead = {
     x: snakeX,
     y: snakeY
