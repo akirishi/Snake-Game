659b504bb0ca0fb34d4f2c003b739f931aa82c46
akirishi <90563712+akirishi@users.noreply.github.com>
2021-10-08 13:34:22 -0300

    Aula 3
    Add: Teleport para o outro lado para não perder a cobrinha

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ snake[0] = {
   x: 8 * box,
   y: 8 * box
 }
-let direction = "right"
+let direction = "right";
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -20,7 +20,22 @@ function criarSnake() {
   }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+  if(event.keyCode == 37 && direction != 'right') direction = "left";
+  if(event.keyCode == 38 && direction != 'down') direction = "up";
+  if(event.keyCode == 39 && direction != 'left') direction = "right";
+  if(event.keyCode == 40 && direction != 'up') direction = "down";
+}
+
 function start() {
+
+  if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+  if(snake[0].x < 0 && direction == 'left') snake[0].x = 15 * box;
+  if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+  if(snake[0].y < 0 && direction == 'up') snake[0].y = 15 * box
+
   criarBG();
   criarSnake();
 
@@ -34,7 +49,7 @@ function start() {
 
   snake.pop();
 
-  letnewHead = {
+  let newHead = {
     x: snakeX,
     y: snakeY
   }
@@ -42,4 +57,4 @@ function start() {
   snake.unshift(newHead);
 }
 
-let jogo = setInterval(start, 100)
+let jogo = setInterval(start, 100);
